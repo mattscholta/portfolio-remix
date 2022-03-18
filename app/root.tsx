17ef9bb318b8933fc5542bea469207e239f068d1
Matthew Scholta <matthew.scholta@gmail.com>
2022-03-18 09:00:10 -0700

    feat: adding a meta tag

--- app/root.tsx
+++ app/root.tsx
@@ -52,7 +52,8 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta content="width=device-width,initial-scale=1" name="viewport" />
+        <meta content="#f00" name="theme-color" />
         <Meta />
 
         <link href={canonnical} rel="canonnical" />
