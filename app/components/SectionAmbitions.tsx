fe17c3a615b87d17436d54ed18d9d4a604f25009
Matt Scholta <matthew.scholta@gmail.com>
2022-07-13 19:03:46 -0700

    📦 Package updates (#57)
    - chore: package updates

--- app/components/SectionAmbitions.tsx
+++ app/components/SectionAmbitions.tsx
@@ -3,10 +3,10 @@ export const SectionAmbitions = () => {
     <section className="mx-auto my-20 max-w-3xl px-4 md:my-40 md:px-0">
       <h2 className="my-4 text-xl md:text-3xl">Ambitions 🎯</h2>
       <p>
-        Over the past 7 years I've been working at startups and I love the
-        flexibility it provides. It's allowed me to shift focus from the
-        Frontend, to Backend, and into DevOps. I'm always looking for{" "}
-        <b className="italic">new challenges</b> and{" "}
+        <b>15+</b> years of professional experience, the past 7 years at
+        startups, and I love the flexibility it provides! It's allowed me to
+        shift focus from the Frontend, to Backend, and into DevOps. I'm always
+        looking for <b className="italic">new challenges</b> and{" "}
         <b className="italic">opportunities</b> to push myself, and the products
         I help create.
       </p>
